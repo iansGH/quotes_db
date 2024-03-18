7057073463f638393ff8997ce4eda302477c4555
iansGH <112134772+iansGH@users.noreply.github.com>
2024-03-18 17:16:18 -0500

    5th commit

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -26,7 +26,7 @@ if(isset($quote->quote)){
 }
 else{
     //no single quote
-    echo json_encode(array("message:" => 'No Quotes Found'));
+    echo json_encode(array('message' => 'No Quotes Found'));
 } 
 
 
