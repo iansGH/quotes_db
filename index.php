89c1f0cc53d5547276234242d31bb42603b87a75
iansGH <112134772+iansGH@users.noreply.github.com>
2024-03-19 13:12:55 -0500

    14th commit

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
-<html lang="en"><head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INF653 Midterm</title>
-    <style>
-    body {
-        background-color: #333;
-        color: whitesmoke;
-    }
-    </style>
+    <title>Midterm REST API</title>
 </head>
-
 <body>
-    <h1>Midterm REST API</h1>
-
-
-</body></html>
+    
+</body>
+</html>
